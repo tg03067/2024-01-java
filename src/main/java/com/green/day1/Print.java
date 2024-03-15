64e7506cbd8cc70ec0bffd30f26319a048510d87
Administrator <tg03067@gmail.com>
2024-03-15 12:32:58 +0900

    first

---
+++ src/main/java/com/green/day1/Print.java
@@ -0,0 +1,17 @@
+package com.green.day1;
+
+//print access >> PrintAccess
+public class Print {
+    public static void main(String[] args){
+        System.out.print("Hello!한글\n"); //한글
+        System.out.print("Hello!\n");
+        System.out.print("Hello!\n"); // 이스케이프 문자
+
+        System.out.println("안녕!\n");
+        System.out.println("안녕!\t안녕");
+        System.out.println("안녕!");
+        System.out.println("안녕!");
+
+        System.out.println("안녕!");
+    }
+}
