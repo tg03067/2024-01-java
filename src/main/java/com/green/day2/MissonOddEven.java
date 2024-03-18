96048da659fd2d3d349e1d4f7a51043352cbe975
Administrator <tg03067@gmail.com>
2024-03-18 17:27:38 +0900

    first

--- src/main/java/com/green/day2/MissonOddEven.java
+++ src/main/java/com/green/day2/MissonOddEven.java
@@ -5,6 +5,10 @@ public class MissonOddEven {
         for(int i=0; i<1; i++){
             int num = (int) (Math.random() * 10) + 1; //1~10
             int num2 = num % 2;
+
+            //정수타입: byte, short, int, long
+            //실수타입: float, double
+
             //예를들어 -- num 만약 5라면 --
             //콘솔에 출력이 "5는 홀수입니다."
 
--- src/main/java/com/green/day2/Var2.java
+++ src/main/java/com/green/day2/Var2.java
@@ -5,6 +5,7 @@ public class Var2 {
     public static void main(String[] agrs){
         //지역변수
         //변수 선언과 동시에 초기화, 지역변수는 초기화 필수!
+        //num = 읽기, = 왼쪽이 쓰기
         int num = 10, num2 = 11, num3 = 0;
         num3 = num + num2;
         //num3 = 10 + 11;
@@ -16,6 +17,7 @@ public class Var2 {
         num3++;
         ++num3;
         ++num3;
+        //대입연산자 없이 증가 ++증감식
         System.out.println(num3);
 
     }
