086db59deba81917ad3778bea180d00867677a9f
Administrator <tg03067@gmail.com>
2024-04-11 14:37:20 +0900

    first

--- src/main/java/com/green/day19/blackjack/Gamer.java
+++ src/main/java/com/green/day19/blackjack/Gamer.java
@@ -15,12 +15,3 @@ public class Gamer  {
         return this.cards;
     }
 }
-
-class GamerTest {
-    public static void main(String[] args){
-        Gamer gamer = new Gamer();
-        gamer.receiveCard(new Card("스페이드", "9"));
-        gamer.receiveCard(new Card("하트", "2"));
-        List<Card> cards = gamer.openCards();
-    }
-}
